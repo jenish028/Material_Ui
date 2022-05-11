6233958f83e00abc907a208be6b368cbc6d7eaae
Dell <jenishdobariya2003@gmail.com>
2022-05-11 19:52:43 +0530

    1.1

--- app/src/main/java/com/example/material_ui/Blue_Appbar.java
+++ app/src/main/java/com/example/material_ui/Blue_Appbar.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class Blue_Appbar extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
